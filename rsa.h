10376a4e2358876299552f8261f3bc98c3fc713b
terrantsh <tanshanhe@foxmail.com>
2018-09-12 09:56:48 +0800

    [change format about .h]

--- rsa.h
+++ rsa.h
@@ -1,9 +1,3 @@
-/*****************************************************************************
-Filename    : rsa.h
-Author      : Terrantsh (tanshanhe@foxmail.com)
-Date        : 2018-8-31 10:31:23
-Description : RSA加密头文件
-*****************************************************************************/
 #ifndef __RSA_H__
 #define __RSA_H__
 
@@ -26,26 +20,25 @@ void generate_rand(uint8_t *block, uint32_t block_len);
 
 typedef struct {
     uint32_t bits;
-    uint8_t  modulus[RSA_MAX_MODULUS_LEN];
-    uint8_t  exponent[RSA_MAX_MODULUS_LEN];
+    uint8_t  modulus		[RSA_MAX_MODULUS_LEN];
+    uint8_t  exponent		[RSA_MAX_MODULUS_LEN];
 } rsa_pk_t;
 
 typedef struct {
     uint32_t bits;
-    uint8_t  modulus[RSA_MAX_MODULUS_LEN];
-    uint8_t  public_exponet[RSA_MAX_MODULUS_LEN];
-    uint8_t  exponent[RSA_MAX_MODULUS_LEN];
-    uint8_t  prime1[RSA_MAX_PRIME_LEN];
-    uint8_t  prime2[RSA_MAX_PRIME_LEN];
-    uint8_t  prime_exponent1[RSA_MAX_PRIME_LEN];
-    uint8_t  prime_exponent2[RSA_MAX_PRIME_LEN];
-    uint8_t  coefficient[RSA_MAX_PRIME_LEN];
+    uint8_t  modulus		[RSA_MAX_MODULUS_LEN];
+    uint8_t  public_exponet	[RSA_MAX_MODULUS_LEN];
+    uint8_t  exponent		[RSA_MAX_MODULUS_LEN];
+    uint8_t  prime1			[RSA_MAX_PRIME_LEN	];
+    uint8_t  prime2			[RSA_MAX_PRIME_LEN	];
+    uint8_t  prime_exponent1[RSA_MAX_PRIME_LEN	];
+    uint8_t  prime_exponent2[RSA_MAX_PRIME_LEN	];
+    uint8_t  coefficient	[RSA_MAX_PRIME_LEN	];
 } rsa_sk_t;
 
 int rsa_public_encrypt (uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_pk_t *pk);
 int rsa_public_decrypt (uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_pk_t *pk);
 int rsa_private_encrypt(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_sk_t *sk);
 int rsa_private_decrypt(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_sk_t *sk);
-
 
 #endif  // __RSA_H__
