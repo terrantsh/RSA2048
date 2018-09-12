8d980daeecd8a4ac877baa23ef43e87949bd469a
terrantsh <tanshanhe@foxmail.com>
2018-09-12 09:55:52 +0800

    [change format]

--- rsa.c
+++ rsa.c
@@ -1,14 +1,7 @@
-/*****************************************************************************
-Filename    : rsa.c
-Author      : Terrantsh (tanshanhe@foxmail.com)
-Date        : 2018-8-31 10:31:23
-Description : RSA加密函数
-*****************************************************************************/
 #include <string.h>
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-
 #include "rsa.h"
 #include "bignum.h"
 
@@ -20,7 +13,7 @@ void generate_rand(uint8_t *block, uint32_t block_len)
     }
 }
 
-static int public_block_operation(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_pk_t *pk);
+static int public_block_operation (uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_pk_t *pk);
 static int private_block_operation(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_sk_t *sk);
 
 int rsa_public_encrypt(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_pk_t *pk)
@@ -164,8 +157,8 @@ static int public_block_operation(uint8_t *out, uint32_t *out_len, uint8_t *in, 
     uint32_t edigits, ndigits;
     bn_t c[BN_MAX_DIGITS], e[BN_MAX_DIGITS], m[BN_MAX_DIGITS], n[BN_MAX_DIGITS];
 
-    bn_decode(m, BN_MAX_DIGITS, in, in_len);
-    bn_decode(n, BN_MAX_DIGITS, pk->modulus, RSA_MAX_MODULUS_LEN);
+    bn_decode(m, BN_MAX_DIGITS, in, 		  in_len			 );
+    bn_decode(n, BN_MAX_DIGITS, pk->modulus,  RSA_MAX_MODULUS_LEN);
     bn_decode(e, BN_MAX_DIGITS, pk->exponent, RSA_MAX_MODULUS_LEN);
 
     ndigits = bn_digits(n, BN_MAX_DIGITS);
@@ -190,17 +183,17 @@ static int public_block_operation(uint8_t *out, uint32_t *out_len, uint8_t *in, 
 static int private_block_operation(uint8_t *out, uint32_t *out_len, uint8_t *in, uint32_t in_len, rsa_sk_t *sk)
 {
     uint32_t cdigits, ndigits, pdigits;
-    bn_t c[BN_MAX_DIGITS], cp[BN_MAX_DIGITS], cq[BN_MAX_DIGITS];
-    bn_t dp[BN_MAX_DIGITS], dq[BN_MAX_DIGITS], mp[BN_MAX_DIGITS], mq[BN_MAX_DIGITS];
-    bn_t n[BN_MAX_DIGITS], p[BN_MAX_DIGITS], q[BN_MAX_DIGITS], q_inv[BN_MAX_DIGITS], t[BN_MAX_DIGITS];
+    bn_t c [BN_MAX_DIGITS], cp[BN_MAX_DIGITS], cq[BN_MAX_DIGITS];
+    bn_t dp[BN_MAX_DIGITS], dq[BN_MAX_DIGITS], mp[BN_MAX_DIGITS], mq   [BN_MAX_DIGITS];
+    bn_t n [BN_MAX_DIGITS], p [BN_MAX_DIGITS], q [BN_MAX_DIGITS], q_inv[BN_MAX_DIGITS], t[BN_MAX_DIGITS];
 
-    bn_decode(c, BN_MAX_DIGITS, in, in_len);
-    bn_decode(n, BN_MAX_DIGITS, sk->modulus, RSA_MAX_MODULUS_LEN);
-    bn_decode(p, BN_MAX_DIGITS, sk->prime1, RSA_MAX_PRIME_LEN);
-    bn_decode(q, BN_MAX_DIGITS, sk->prime2, RSA_MAX_PRIME_LEN);
-    bn_decode(dp, BN_MAX_DIGITS, sk->prime_exponent1, RSA_MAX_PRIME_LEN);
-    bn_decode(dq, BN_MAX_DIGITS, sk->prime_exponent2, RSA_MAX_PRIME_LEN);
-    bn_decode(q_inv, BN_MAX_DIGITS, sk->coefficient, RSA_MAX_PRIME_LEN);
+    bn_decode(c,     BN_MAX_DIGITS, in, 				 in_len				);
+    bn_decode(n,  	 BN_MAX_DIGITS, sk->modulus, 		 RSA_MAX_MODULUS_LEN);
+    bn_decode(p,  	 BN_MAX_DIGITS, sk->prime1,  		 RSA_MAX_PRIME_LEN  );
+    bn_decode(q, 	 BN_MAX_DIGITS, sk->prime2, 		 RSA_MAX_PRIME_LEN	);
+    bn_decode(dp, 	 BN_MAX_DIGITS, sk->prime_exponent1, RSA_MAX_PRIME_LEN	);
+    bn_decode(dq,    BN_MAX_DIGITS, sk->prime_exponent2, RSA_MAX_PRIME_LEN	);
+    bn_decode(q_inv, BN_MAX_DIGITS, sk->coefficient, 	 RSA_MAX_PRIME_LEN	);
 
     cdigits = bn_digits(c, BN_MAX_DIGITS);
     ndigits = bn_digits(n, BN_MAX_DIGITS);
@@ -212,35 +205,35 @@ static int private_block_operation(uint8_t *out, uint32_t *out_len, uint8_t *in,
     bn_mod(cp, c, cdigits, p, pdigits);
     bn_mod(cq, c, cdigits, q, pdigits);
     bn_mod_exp(mp, cp, dp, pdigits, p, pdigits);
-    bn_assign_zero(mq, ndigits);
     bn_mod_exp(mq, cq, dq, pdigits, q, pdigits);
+    bn_assign_zero(mq, ndigits);
 
     if(bn_cmp(mp, mq, pdigits) >= 0) {
         bn_sub(t, mp, mq, pdigits);
     } else {
         bn_sub(t, mq, mp, pdigits);
-        bn_sub(t, p, t, pdigits);
+        bn_sub(t, p , t , pdigits);
     }
 
     bn_mod_mul(t, t, q_inv, p, pdigits);
-    bn_mul(t, t, q, pdigits);
+    bn_mul(t, t, q , pdigits);
     bn_add(t, t, mq, ndigits);
 
     *out_len = (sk->bits + 7) / 8;
     bn_encode(out, *out_len, t, ndigits);
 
     // Clear potentially sensitive information
-    memset((uint8_t *)c, 0, sizeof(c));
-    memset((uint8_t *)cp, 0, sizeof(cp));
-    memset((uint8_t *)cq, 0, sizeof(cq));
-    memset((uint8_t *)dp, 0, sizeof(dp));
-    memset((uint8_t *)dq, 0, sizeof(dq));
-    memset((uint8_t *)mp, 0, sizeof(mp));
-    memset((uint8_t *)mq, 0, sizeof(mq));
-    memset((uint8_t *)p, 0, sizeof(p));
-    memset((uint8_t *)q, 0, sizeof(q));
+    memset((uint8_t *)c,  	 0, sizeof(c ));
+    memset((uint8_t *)cp, 	 0, sizeof(cp));
+    memset((uint8_t *)cq, 	 0, sizeof(cq));
+    memset((uint8_t *)dp, 	 0, sizeof(dp));
+    memset((uint8_t *)dq,	 0, sizeof(dq));
+    memset((uint8_t *)mp, 	 0, sizeof(mp));
+    memset((uint8_t *)mq, 	 0, sizeof(mq));
+    memset((uint8_t *)p,  	 0, sizeof(p ));
+    memset((uint8_t *)q,  	 0, sizeof(q ));
     memset((uint8_t *)q_inv, 0, sizeof(q_inv));
-    memset((uint8_t *)t, 0, sizeof(t));
+    memset((uint8_t *)t,  	 0, sizeof(t ));
 
     return 0;
 }
