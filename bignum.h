81219bcdf095d8cb6800bc3cb7a89ae689c15f19
tanshanhe <tanshanhe@foxmail.com>
2018-09-20 11:24:05 +0800

    [master] Change the mistake
    4097

--- bignum.h
+++ bignum.h
@@ -1,7 +1,13 @@
-#include <stdint.h>
-
+/*****************************************************************************
+Filename    : bignum.h
+Author      : Terrantsh (tanshanhe@foxmail.com)
+Date        : 2018-9-20 11:23:45
+Description :
+*****************************************************************************/
 #ifndef __BIGNUM_H__
 #define __BIGNUM_H__
+
+#include <stdint.h>
 
 typedef uint64_t dbn_t;
 typedef uint32_t bn_t;
@@ -12,6 +18,7 @@ typedef uint32_t bn_t;
 #define BN_MAX_DIGIT                0xFFFFFFFF
 
 #define DIGIT_2MSB(x)               (uint32_t)(((x) >> (BN_DIGIT_BITS - 2)) & 0x03)
+
 
 void bn_decode(bn_t *bn, uint32_t digits, uint8_t *hexarr, uint32_t size);
 void bn_encode(uint8_t *hexarr, uint32_t size, bn_t *bn, uint32_t digits);
@@ -31,6 +38,7 @@ void bn_mod_mul(bn_t *a, bn_t *b, bn_t *c, bn_t *d, uint32_t digits);           
 void bn_mod_exp(bn_t *a, bn_t *b, bn_t *c, uint32_t cdigits, bn_t *d, uint32_t ddigits);    // a = b ^ c mod d
 
 int bn_cmp(bn_t *a, bn_t *b, uint32_t digits);                                              // returns sign of a - b
+
 uint32_t bn_digits(bn_t *a, uint32_t digits);                                               // returns significant length of a in digits
 
 #define BN_ASSIGN_DIGIT(a, b, digits)   {bn_assign_zero(a, digits); a[0] = b;}
